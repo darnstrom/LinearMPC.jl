5c0bbd831263a530fd9f7ec2cfc40180db292782
Daniel Arnström <daniel.arnstrom@liu.se>
2022-10-21 14:17:20 +0200

    Extend example

--- example/codegen.jl
+++ example/codegen.jl
@@ -1,3 +1,7 @@
 using LinearMPC
 mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend",double_sided=true)
 LinearMPC.codegen(mpc)
+
+## BnB
+mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",5,5,double_sided=true)
+LinearMPC.codegen(mpc)
