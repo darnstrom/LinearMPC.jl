f9d8f9fe801c83fac06a719155794596505091ae
Daniel Arnström <daniel.arnstrom@liu.se>
2022-10-28 16:45:40 +0200

    Add JuMP model option.

--- src/utils.jl
+++ src/utils.jl
@@ -21,8 +21,29 @@ function compute_control(mpc::MPC,θ)
         bu = mpQP.bu + bth 
         bl = mpQP.bl + bth 
         f = mpQP.f +mpQP.f_theta*θ
-        u,~,exitflag,info = DAQP.quadprog(mpQP.H,f[:],mpQP.A,bu[:],bl[:],mpQP.senses)
-        println(exitflag)
+        d = DAQP.Model()
+        DAQP.setup(d,mpQP.H,f[:],mpQP.A,bu[:],bl[:],mpQP.senses[:])
+        DAQP.settings(d,Dict(:iter_limit=>1e4))
+        DAQP.settings(d,Dict(:cycle_tol=>25))
+        DAQP.settings(d,Dict(:primal_tol=>1e-6))
+        DAQP.settings(d,Dict(:pivot_tol=>1e-4))
+        udaqp,~,exitflag,info = DAQP.solve(d)
+        #println(info)
+
+        # Test jump 
+        model = create_jump(mpQP.H,f[:],mpQP.A,bu[:],bl[:],findall(mpQP.senses .== 16))
+        optimize!(model)
+        #display(solution_summary(model))
+        u = value.(model[:x])
+        #u,~,exitflag,info = DAQP.quadprog(mpQP.H,f[:],mpQP.A,bu[:],bl[:],mpQP.senses[:])
+        println("flag: $exitflag, iterations: $(info.iterations), nodes: $(info.nodes)")
+        println("DIFF: $(norm(u-udaqp))")
+        if(exitflag != 1)
+            #return;
+            #mpQP.senses[:].=0;
+            #u,~,exitflag,info = DAQP.quadprog(mpQP.H,f[:],mpQP.A,bu[:],bl[:],mpQP.senses)
+            #println("Completely relaxed: $exitflag") 
+        end
     end
     return u[1:mpc.nu]
 end
@@ -61,4 +82,23 @@ function simulate(mpc::MPC,x0,N_steps;r=nothing)
         xs[:,i+1] = x
     end
     return xs,us,rs
+end
+
+function create_jump(H,f,A,bu,bl,bin_ids)
+    n = length(f)
+    nb = length(bu)-size(A,1)
+    if(n > nb)
+        bl = [bl[1:nb];-1e30*ones(n-nb);bl[nb+1:end]]
+        bu = [bu[1:nb];1e30*ones(n-nb);bu[nb+1:end]]
+    end
+    model = direct_model(Gurobi.Optimizer())
+    #set_optimizer_attribute(model, "OutputFlag", 0)
+    @variable(model, bl[i] <= x[i = 1:n] <= bu[i])
+    for i in bin_ids
+        set_binary(x[i])
+    end
+    @objective(model, Min, 0.5*x'*H*x+f'*x)
+    @constraint(model, A*x .<= bu[n+1:end])
+    @constraint(model, A*x .>= bl[n+1:end])
+    return model
 end
