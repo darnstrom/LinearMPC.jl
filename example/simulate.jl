a4f3307cb2180d36207c00072030c20d8f1024ec
Daniel Arnström <daniel.arnstrom@liu.se>
2022-10-28 16:46:50 +0200

    Update example.

--- example/simulate.jl
+++ example/simulate.jl
@@ -4,19 +4,25 @@ using LinearMPC
 opts = LinearMPC.MPCSettings()
 mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend",settings=opts)
 
-x0 = [10.0,0.0,0.0,0.0,1]; 
-rs = [zeros(2,50) repeat([10;0],1,250)];
-xs,us,rs = LinearMPC.simulate(mpc,x0,3000;r=rs)
+x0 = [0.0,0.0,0.0,0.0,-1]; 
+rs = [zeros(2,20) repeat([20;0],1,80)];
+xs,us,rs = LinearMPC.simulate(mpc,x0,1000;r=rs)
 
 ## Contact invpend 
+using LinearMPC
+using UnicodePlots
+Nsim = 50 
 
-mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",10,10)
+#opts = LinearMPC.MPCSettings()
+#opts.reference_tracking = false
+#opts.explicit_soft = true 
+#mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",5,5;settings=opts)
+mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",5,5)
 
 x0 = [0.0, 0.0, 1.0, 0.0]; 
 #rs = [zeros(2,50) repeat([10;0],1,250)];
 #xs,us,rs = LinearMPC.simulate(mpc,x0,3000;r=rs)
-xs,us,rs = LinearMPC.simulate(mpc,x0,30)
+xs,us,rs = LinearMPC.simulate(mpc,x0,Nsim)
 
-Nmax = 30
-plt = lineplot(xs[1,1:Nmax] .- xs[2,1:Nmax] .- 0.5)
-plt = lineplot(-xs[1,1:Nmax] .+ xs[2,1:Nmax] .- 0.5)
+plt = lineplot(xs[1,1:Nsim] .- xs[2,1:Nsim] .- 0.5)
+plt = lineplot(-xs[1,1:Nsim] .+ xs[2,1:Nsim] .- 0.5)
