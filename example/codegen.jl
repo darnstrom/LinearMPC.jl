129ef046bcdff327e911883f435128ca76265911
Daniel Arnström <daniel.arnstrom@liu.se>
2022-11-04 16:18:24 +0100

    Change optional arguments in codegen.

--- example/codegen.jl
+++ example/codegen.jl
@@ -1,7 +1,8 @@
 using LinearMPC
-mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend",double_sided=true)
+mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend")
+mpc.settings.QP_double_sided = true
 LinearMPC.codegen(mpc)
 
 ## BnB
-mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",5,5,double_sided=true)
+mpQP,P_theta,mpc = LinearMPC.mpc_examples("invpend_contact",5,5)
 LinearMPC.codegen(mpc)
--- src/codegen.jl
+++ src/codegen.jl
@@ -1,10 +1,13 @@
 using DAQP
 
-function codegen(mpc;fname="mpc_workspace",explicit_soft=false,reference_tracking=false)
+function codegen(mpc;fname="mpc_workspace", opt_settings=nothing)
     # Generate QP workspace
-    mpQP = mpc2mpqp(mpc;explicit_soft,reference_tracking)
+    mpQP = mpc2mpqp(mpc)
     d = DAQP.Model()
-    DAQP.setup(d,mpQP.H,mpQP.f[:],mpQP.A,mpQP.bu[:],mpQP.bl[:],mpQP.senses)
+    DAQP.setup(d,mpQP.H[:,:],mpQP.f[:],mpQP.A[:,:],mpQP.bu[:],mpQP.bl[:],mpQP.senses[:])
+    if(!isnothing(opt_settings))
+        DAQP.settings(d,opt_settings)
+    end
     DAQP.codegen(d;filename=fname)
 
     # Append MPC-specific data/functions
