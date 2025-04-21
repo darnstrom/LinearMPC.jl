75ec703d294af35e86cfb8c4150282417676b447
Documenter.jl <documenter@juliadocs.github.io>
2025-04-21 20:31:52 +0000

    build based on c41f0dc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LinearMPC.jl","page":"Home","title":"LinearMPC.jl","text":"","category":"section"},{"location":"#Functions","page":"Home","title":"Functions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [LinearMPC]","category":"page"},{"location":"#LinearMPC.mpc2mpqp-Tuple{LinearMPC.MPC}","page":"Home","title":"LinearMPC.mpc2mpqp","text":"mpc2mpqp(mpc)\n\nFor a given MPC structure mpc, form the multi-parametric QP mpQP. \n\n\n\n\n\n","category":"method"}]
+}
