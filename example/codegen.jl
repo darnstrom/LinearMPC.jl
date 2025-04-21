faaaef7f0aacf3c2cbca17562c60fe5efa1fcabd
Daniel Arnström <daniel.arnstrom@liu.se>
2025-04-21 11:50:34 +0200

    Add some tests

--- example/codegen.jl
+++ example/codegen.jl
@@ -2,6 +2,7 @@ using LinearMPC
 # Load inverted pendulum example and generate code 
 mpc,range = LinearMPC.mpc_examples("invpend")
 LinearMPC.codegen(mpc;dir="codegen_impc")
+
 # Compute explicit 
 empc = LinearMPC.ExplicitMPC(mpc;range)
 LinearMPC.codegen(empc;dir="codegen_empc")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,48 @@
 using LinearMPC
 using Test
+using LinearAlgebra
+global templib
 
-@testset "LinearMPC.jl" begin
-  mpc,range = LinearMPC.mpc_examples("invpend");
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("dcmotor");
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("aircraft");
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("nonlin");
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("mass", 10,10,params=Dict(:nx=>2));
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("chained",10,10,params=Dict(:nx=>2));
-  LinearMPC.mpc2mpqp(mpc)
-  mpc,range = LinearMPC.mpc_examples("invpend_contact",6,6, params=Dict(:nwalls=>1));
-  LinearMPC.mpc2mpqp(mpc)
+@testset verbose = true "LinearMPC.jl" begin
+    @testset "MPC examples " begin
+        mpc,range = LinearMPC.mpc_examples("invpend");
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("dcmotor");
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("aircraft");
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("nonlin");
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("mass", 10,10,params=Dict(:nx=>2));
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("chained",10,10,params=Dict(:nx=>2));
+        LinearMPC.mpc2mpqp(mpc)
+        mpc,range = LinearMPC.mpc_examples("invpend_contact",6,6, params=Dict(:nwalls=>1));
+        LinearMPC.mpc2mpqp(mpc)
+    end
+ 
+    @testset "Compute control" begin
+        mpc,range = LinearMPC.mpc_examples("invpend")
+        control = LinearMPC.compute_control(mpc,[5.0;5;0;0])
+        @test norm(control.-1.7612519326) < 1e-6
+    end
+
+    @testset "Codegen IMPC" begin
+        mpc,range = LinearMPC.mpc_examples("invpend")
+        srcdir = tempname()
+        LinearMPC.codegen(mpc;dir=srcdir)
+        src = [f for f in readdir(srcdir) if last(f,1) == "c" && f != "example.c"]
+        testlib = "mpctest."* Base.Libc.Libdl.dlext
+        if(!isnothing(Sys.which("gcc")))
+            run(Cmd(`gcc -lm -fPIC -O3 -msse3 -xc -shared -o $testlib $src`; dir=srcdir))
+        end
+        theta = [5;5;zeros(5)]
+        control = ones(1)
+        global templib = joinpath(srcdir,testlib)
+        ccall(("mpc_compute_control", templib), Cint, (Ptr{Cdouble}, Ptr{Cdouble}), theta, control)
+        @test norm(control.-1.7612519326) < 1e-6
+    end
+
+    # TODO: add codegen EMPC test
+
 end
