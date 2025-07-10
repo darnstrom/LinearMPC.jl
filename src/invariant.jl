e1ea541f4af1cd9504302a4f24b4142cc0c02883
Daniel Arnström <daniel.arnstrom@liu.se>
2025-07-10 17:16:47 +0200

    Add function for invariant set for MPC

--- src/invariant.jl
+++ src/invariant.jl
@@ -1,9 +1,64 @@
+function cis(mpc::MPC;xmin=zeros(0),xmax=zeros(0),wmin=zeros(0),wmax=zeros(0),max_iter=500,eps_shrink=1e-3,verbose=true)
+    F,G = mpc.model.F,mpc.model.G
+    nx,nu = size(G)
+    umin,umax = mpc.umin,mpc.umax
+    Ax,bx = zeros(0,nx),zeros(0)
+    Agu,Agx,bg = zeros(0,nu),zeros(0,nx),zeros(0)
+    #  Extract constraint into general form
+    # TODO : account for special constraints and ks
+    # TODO : account for Ar,Aw,Ad,Aup...
+    for c in mpc.constraints
+        if(!isempty(c.Ax))
+            if(!isempty(c.Au))
+                Agu,Agx,bg = [Agu;c.Au;-c.Au],[Agx;c.Ax;-c.Ax],[bg;c.ub;-c.lb]
+            else
+                Ax,bx = [Ax;c.Ax;-c.Ax],[bx;c.ub;-c.lb]
+            end
+        else
+            if(!isempty(c.Au))
+                Agu,Agx,bg = [Agu;c.Au;-c.Au],[Agx;zeros(2*length(c.ub),nx)],[bg;c.ub;-c.lb]
+            end
+        end
+    end
+    # TODO prune inf bounds
+    invariant_set(F,xmin,xmax;wmax,wmin,G,umin,umax,Ax,bx,Agu,Agx,bg,max_iter,eps_shrink)
+end
+function invariant_set(mpc::MPC,K=zeros(0,0);xmin=zeros(0),xmax=zeros(0),wmin=zeros(0),wmax=zeros(0),max_iter=500,eps_shrink=1e-3,verbose=true)
+    F,G = mpc.model.F,mpc.model.G
+    if(isempty(K))
+        K = -mpc.K
+    end
+    nx,nu = size(G)
+    umin,umax = mpc.umin,mpc.umax
+    Ax,bx = zeros(0,nx),zeros(0)
+    #  Extract constraint into general form
+    # TODO : account for special constraints and ks
+    # TODO : account for Ar,Aw,Ad,Aup...
+    for c in mpc.constraints
+        if(!isempty(c.Ax))
+            if(!isempty(c.Au))
+                Ax,bx = [Ax;c.Ax+c.Au*K;-c.Ax-c.Au*K],[bx;c.ub;-c.lb]
+            else
+                Ax,bx = [Ax;c.Ax;-c.Ax],[bx;c.ub;-c.lb]
+            end
+        else
+            if(!isempty(c.Au))
+                Ax,bx = [Ax;c.Au*K;-c.Au*K],[bx;c.ub;-c.lb]
+            end
+        end
+    end
+    if(!isempty(umin))
+        Ax,bx = [Ax;I(nu)*K;-I(nu)*K],[bx;umax;-umin]
+    end
+    # TODO prune inf bounds
+    invariant_set(F,xmin,xmax;wmax,wmin,Ax,bx,max_iter,eps_shrink)
+end
 function invariant_set(F::Matrix{<:Real},xmin=zeros(0),xmax=zeros(0);
               wmin=zeros(0),wmax=zeros(0),
               G=zeros(0),umin=zeros(0),umax=zeros(0),
               Ax=zeros(0),bx=zeros(0),
               Agu=zeros(0,0),Agx=zeros(0,0),bg=zeros(0),
-              max_iter=500, eps_shrink=1e-3)
+              max_iter=500, eps_shrink=1e-3, verbose=true)
 
     # Setup the sets X = {x : H*x ≤ h }, U = {u : Hu*u ≤ hu}
     nx,nu = size(F,1),size(G,2)
@@ -35,6 +90,7 @@ function invariant_set(F::Matrix{<:Real},xmin=zeros(0),xmax=zeros(0);
 
     # Start the iterations
     for iter in 1:max_iter
+        verbose && println("\r>> #$iter | Constraints: $(length(h))        ");
         hadd = copy(h)
         if(!isempty(wmax))
             # compute hi - max_{w∈W} hi'*w (where W is a box => explicit solution)
@@ -57,5 +113,6 @@ function invariant_set(F::Matrix{<:Real},xmin=zeros(0),xmax=zeros(0);
         h[nold+1:end].-=eps_shrink # shrink new constraints
         H,h = minrep(H, h; keep=nold+1:length(h))
     end
+    verbose && println("")
     return H,h
 end
