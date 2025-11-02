1ad2744a976a32c9c19aff0cd77794ce22795472
Daniel Arnström <daniel.arnstrom@gmail.com>
2025-11-02 15:31:07 +0100

    Add test for offset

--- test/runtests.jl
+++ test/runtests.jl
@@ -540,13 +540,13 @@ global templib
     end
     @testset "Constant offset" begin
         F,G = [1 0.1; 0 1], [0.005;0.1;;] # double integrator with Ts=0.1
-        mpc= LinearMPC.MPC(F,G;Ts=0.1,Np=25,C=[1 0;])
+        mpc= LinearMPC.MPC(F,G;Ts=0.1,Np=25,C=[1 0;], h = [0.1;0.1])
         set_objective!(mpc,R=0,Rr=1,Q=1)
-        mpc.model.h .= [0.1;0.1]
         set_bounds!(mpc;umin=[-2],umax=[2],ymin=[-0.5],ymax=[0.5])
         LinearMPC.set_state_observer!(mpc;Q=1e-3)
         dynamics = (x,u,d) -> mpc.model.F*x + mpc.model.G*u + [0.1;0.1]
         get_measurement = (x,d) -> mpc.state_observer.C*x + 0.01*randn(1)
         sim= Simulation(dynamics,mpc;r=[0.5], get_measurement)
+        @test all(abs.(sim.xs[1,end-50:end].-0.5) .< 0.1) # Check if x1 converged to 0.5 
     end
 end
