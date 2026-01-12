e216dc9b779c4e9b534ac6fedaec888bab54f69f
Daniel Arnström <daniel.arnstrom@gmail.com>
2026-01-12 21:38:33 +0100

    Update C functions

--- codegen/mpc_observer.h
+++ codegen/mpc_observer.h
@@ -1,2 +1,2 @@
-void mpc_predict_state(c_float* state, c_float* control);
-void mpc_correct_state(c_float* state, c_float* measurement);
+void mpc_predict_state(c_float* state, c_float* control, c_float* disturbance);
+void mpc_correct_state(c_float* state, c_float* measurement, c_float* distrubance);
