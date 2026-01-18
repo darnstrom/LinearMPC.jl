cbaa06cfc1e9da268f7325718cdc21405d4df7dd
Daniel Arnström <55484604+darnstrom@users.noreply.github.com>
2026-01-18 19:08:27 +0100

    Add precompile for setup (#77)

--- src/LinearMPC.jl
+++ src/LinearMPC.jl
@@ -50,4 +50,20 @@ include("observer.jl");
 export predict_state!,correct_state!
 export set_state!,get_state,update_state!
 
+using PrecompileTools
+@setup_workload begin
+
+    F = [1 0.1; 0 1];
+    G = [0 0;1 1];
+    @compile_workload begin
+        mpc = LinearMPC.MPC(F,G;C=[1 0;0 1],Np=10);
+        set_objective!(mpc; Q=[1,1], R=[0,0], Rr=1e3);
+        set_bounds!(mpc;umin=-ones(2),umax=ones(2));
+        add_constraint!(mpc;Ax = [1 0], lb=[8],ub=[10],soft=true)
+        move_block!(mpc,[1,1,8])
+        setup!(mpc)
+        sim = Simulation(mpc;x0=10*ones(2), r = [10,0], N=100);
+    end
+end
+
 end
